1f685fb6dad09b381058e025e392044b265f64f0
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-04-06 21:24:31 -0300

    feat: created stars animation

---
+++ src/components/Stars/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+import { THEME } from '../../styles/theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  canvas: {
+    width: 257,
+    height: 249,
+    position: 'absolute',
+    zIndex: 1
+  }
+});
